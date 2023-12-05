5369e6a70fe37077b68839407a812d3a497e0404
sahrullugianabadri <sahrullugianabadri61@gmail.com>
2023-12-05 16:00:41 +0700

    merubah main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       translations: AppLocalization(),
       locale: Get.deviceLocale, //for setting localization strings
       fallbackLocale: Locale('en', 'US'),
-      title: 'tripmate_ori',
+      title: 'TripMate',
       initialBinding: InitialBindings(),
       initialRoute: AppRoutes.initialRoute,
       getPages: AppRoutes.pages,
